9964fe7b56870f52d1629024df0bd3bb33389dcc
hayden <48267247+hayden-cn@users.noreply.github.com>
2026-05-27 13:29:13 +0800

    chore: 更新上游仓库地址

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "hayden-fr/scoop-bucket:main"
+    [String]$upstream = "hayden-cn/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
